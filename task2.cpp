a2fbedfa0b8be8d8808aa86ea92243f0377e06a0
Daniil-main <66488189+Daniil-main@users.noreply.github.com>
2026-02-16 21:13:34 +0300

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+int main()
+{
+    long long int n, m, a, ans;
+    cin >> n >> m >> a;
+    ans = ((n + a - 1) / a) * ((m + a - 1) / a);
+    cout << ans;
+    return 0;
+}
